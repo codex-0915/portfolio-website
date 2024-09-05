285cc16a65e6563d3a4752299651e62dc9a502b0
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-09-05 22:15:54 +0800

    Add hero section texts with effect

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
+import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
@@ -23,8 +24,15 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Testing Heading Output
+            Enjoying Science & Tech
           </h2>
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Turning Challenges into Code: Crafting Solutions That Drive Success"
+          />
+          <p className="text-center md:tracking-wider mb-4 text-small md:text-lg lg:text-2xl">
+            Hi, I&apos;m Kyl, a Software Engineer based in the Philippines.
+          </p>
         </div>
       </div>
     </div>
