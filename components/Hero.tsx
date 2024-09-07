f4a262151315705cae121f6a5564ec1be19df20a
Kyl Justin Perez <88893914+codex-0915@users.noreply.github.com>
2024-09-07 21:51:24 +0800

    Add magic button on hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import { MagicButton } from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
 
 const Hero = () => {
   return (
@@ -33,6 +35,14 @@ const Hero = () => {
           <p className="text-center md:tracking-wider mb-4 text-small md:text-lg lg:text-2xl">
             Hi, I&apos;m Kyl, a Software Engineer based in the Philippines.
           </p>
+
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
